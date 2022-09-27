6805c99145c0d8c911ecd6f09c65ade456849376
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2022-09-27 15:17:13 -0700

    made descrptive changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,11 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj.XboxController;
+
+
 import frc.robot.Intake.Intake;
+import frc.robot.DriveTrain.TankControl;
 import frc.robot.DriveTrain.DriveTrain;
 import frc.robot.Indexer.Indexer;
 import frc.robot.Shooter.Shooter;
@@ -10,4 +15,9 @@ public class RobotContainer {
     Indexer indexer = new Indexer();
     Shooter shooter = new Shooter();
     DriveTrain driverTrain = new DriveTrain();
+    XboxController controller = new XboxController(0);
+    RobotContainer() {
+        driverTrain.setDefaultCommand(new TankControl(driverTrain));
+        new Trigger(() -> controller.getLeftBumper()).whenActive(() -> {intake.commandSetFold(false);indexer.setPercent(1);shooter.setPercent(1);}).whenInactive(() -> {intake.commandSetFold(true);indexer.stop();shooter.stop();});    
+    }
 }
